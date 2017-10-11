0ea3298389057da012eb2462b7511ee4c78ff0ab
Matt Dickson <matthew.d.dickson@duke.edu>
2017-10-10 23:20:10 -0400

    New table layout reflected in SQL

--- src/sql/create-flights.sql
+++ src/sql/create-flights.sql
@@ -7,23 +7,65 @@ CREATE TABLE Airport
   PRIMARY KEY(callsign)
 );
 
-CREATE TABLE Plane
-( depart_time TIMESTAMP NOT NULL,
-  arrive VARCHAR(3) REFERENCES Airport(callsign) NOT NULL,
-  depart VARCHAR(3) REFERENCES Airport(callsign) NOT NULL,
-  carrier VARCHAR(64) NOT NULL,
-  PRIMARY KEY(depart_time, arrive, depart)
+CREATE TABLE People
+( id INTEGER NOT NULL,
+  username VARCHAR(128) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT positive_id CHECK (
+    id > 0
+  ),
+  -- Don't store data that might be malicious
+  CONSTRAINT second_order_injection_stopper CHECK (
+    username NOT LIKE '%''%'
+ )
 );
 
-CREATE TABLE Ticket
-( arrive VARCHAR(3) REFERENCES Plane(arrive) NOT NULL,
-  depart VARCHAR(3) REFERENCES Plane(depart) NOT NULL,
-  depart_time TIMESTAMP REFERENCES Plane(depart_time) NOT NULL,
+CREATE TABLE Flight
+( flight_code VARCHAR(32) NOT NULL,
+  depart_time TIMESTAMP NOT NULL,
+  arrive VARCHAR(3) REFERENCES Airport(callsign) NOT NULL,
+  depart VARCHAR(3) REFERENCES Airport(callsign) NOT NULL,
+  carrier VARCHAR(64) REFERENCES Airline(name) NOT NULL,
+  PRIMARY KEY(flight_code, depart_time)
+);
+
+CREATE TABLE ConnectingFlight
+( trip_id INTEGER REFERENCES Trip(id) NOT NULL,
+  flight_code VARCHAR(32) REFERENCES Flight(flight_code) NOT NULL,
+  depart_time TIMESTAMP REFERENCES Flight(depart_time) NOT NULL,
+  PRIMARY KEY (trip_id, flight_code, depart_time)
+);
+
+CREATE TABLE Trip
+( id NUMERIC(20) NOT NULL,
   booking_date DATE NOT NULL,
   price FLOAT NOT NULL,
   occupant_age VARCHAR(6) NOT NULL,
   seat_class VARCHAR(5) NOT NULL,
   refundable BOOLEAN NOT NULL,
-  PRIMARY KEY(arrive, depart, depart_time, booking_date,
-              occupant_age, seat_class, refundable)
+  PRIMARY KEY (id),
+  CONSTRAINT positive_id CHECK(
+    id > 0
+  ),
+  CONSTRAINT positive_price CHECK (
+    price > 0
+  ),
+  CONSTRAINT seat_exists CHECK(
+    seat_class IN ('first', 'coach')
+  ),
+  CONSTRAINT avail_ages CHECK (
+    occupant_age IN ('child', 'adult', 'senior')
+  )
+);
+
+CREATE TABLE TripTaker
+( uid INTEGER REFERENCES People(id) NOT NULL,
+  tid INTEGER REFERENCES Trip(id) NOT NULL,
+  PRIMARY KEY (uid, tid)
+);
+
+CREATE TABLE Airline
+( id INTEGER NOT NULL,
+  name VARCHAR(64) NOT NULL,
+  PRIMARY KEY (id)
 );
