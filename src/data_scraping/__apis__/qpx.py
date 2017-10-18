ce83220596c2e885f7b13acc9fce5bb3886d5a67
Matt Dickson <matthew.d.dickson@duke.edu>
2017-10-17 20:33:11 -0400

    Scraper gets/parses information

---
+++ src/data_scraping/__apis__/qpx.py
@@ -0,0 +1,55 @@
+import os
+import json
+import requests
+
+class QPX:
+
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self._url_api = self._get_api_url()
+        self.header = {'Content-type': 'application/json'}
+
+    def make_request(self, depart, arrive, date):
+        self._package_request(depart, arrive, date)
+        self._send_requests()
+
+    def _get_api_url(self):
+        key_location = os.path.join(self.data_path, 'api_url.json')
+        with open(key_location, 'r') as key_file:
+            return json.load(key_file)['url']
+
+    def _package_request(self, depart, arrive, date):
+        self.request = {'request': {
+            'passengers': {
+                'adultCount': 1
+            },
+            'slice': [
+                {
+                'origin': depart,
+                'destination': arrive,
+                'date': date
+            }],
+            'solutions': 30
+            }
+        }
+
+    def _send_requests(self):
+        r = requests.post(self._url_api, data=json.dumps(self.request), headers=self.header)
+        x = r.json()
+        for temp in x['trips']['tripOption']:
+            # print(temp)
+            id = temp['id']
+            total = temp['saleTotal']
+            flight_code = []
+            aircrafts = []
+            airports = []
+            depart_times = []
+            for __slice in temp['slice']:
+                for segment in __slice['segment']:
+                    flight_code.append(segment['flight']['carrier'] + segment['flight']['number'])
+                    leg = segment['leg'][0]
+                    aircrafts.append(leg['aircraft'])
+                    airports.append(leg['origin'])
+                    airports.append(leg['destination'])
+                    depart_times.append(leg['departureTime'])
+            print(id, total, flight_code, aircrafts, airports, depart_times)
--- src/data_scraping/scraper.py
+++ src/data_scraping/scraper.py
@@ -1,8 +1,8 @@
 import json
-import requests
-import records
 import os
-from .textme import Text
+
+from src.data_scraping.__apis__.qpx import QPX
+
 
 def make_route_dict():
     with open(trip_routes, 'r') as routes_file:
@@ -10,30 +10,22 @@ def make_route_dict():
         for flight in data['Flight']:
             route_dict[flight['depart']] = flight['arrive']
 
-
-def generate_request():
-    
-
-
-
-def get_api_url():
-    key_location = os.path.join(data_path, 'api_url.json')
-    with open(key_location, 'r') as key_file:
-        return json.load(key_file)['url']
-
-
 def get_db_url():
     file_loaction = os.path.join(data_path, 'db_url.json')
     with json.load(open(file_loaction)) as data_file:
+        return data_file['url']
 
 
 if __name__ == '__main__':
     data_path = os.path.abspath(os.path.join(os.getcwd(), "..", "..", "data"))
     trip_routes = os.path.join(data_path, 'TripRoutes.json')
-    api_url = get_api_url()
-    db_url = get_db_url()
-    route_dict = {}
 
-    make_route_dict()
-    generate_request()
-    text_interface = Text(data_path)
+    # text_api = Text(data_path)
+    flight_api = QPX(data_path)
+
+    flight_api.make_request('SAN', 'RDU', '2017-11-10')
+
+    #db_url = get_db_url()
+    #route_dict = {}
+
+    #make_route_dict()
