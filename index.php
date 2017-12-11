e1d949f1643393bd226edcf5602e5b507380d075
Natalie Huffman <natalie.huffman@duke.edu>
2017-12-10 20:42:13 -0500

    moved index outside src to facilitate website hosting

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+<title>Flight Data</title>
+</head>
+
+<body>
+
+<center><h1>Find When to Book Your Flight</h1></center>
+
+<p>
+<a href="cities.php?direction=<?= True ?>">Find a flight departing from RDU</a> <br>
+<a href="cities.php?direction=<?= False?>">Find a flight arriving at RDU</a>
+</p>
+
+</body>
+</html>
